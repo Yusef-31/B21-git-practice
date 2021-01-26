5a64229bb285f3757d43b0f6094b07168a51cf1f
Yusef-31 <74149510+Yusef-31@users.noreply.github.com>
2021-01-26 14:10:08 -0500

    Tester changes

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,2 +1,11 @@
 public class HelloTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Hello Test! ");
+
+        System.out.println(" Hello testers!");
+
+        System.out.println(" I am a tester!");
+    }
 }
